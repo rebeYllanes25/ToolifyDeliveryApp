677ae7e0713ccf7478a25a96ec6f228e1bb4e2a3
Vector404 <victornarazas2005@gmail.com>
2025-10-08 17:43:32 -0500

    Terminando vista seguimiento

--- app/src/main/java/com/cibertec/proyectodami/MainActivity.kt
+++ app/src/main/java/com/cibertec/proyectodami/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.cibertec.proyectodami
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.cibertec.proyectodami.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnNuevoPedido.setOnClickListener{
+
+            val intent = Intent(this, SeguimientoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/cibertec/proyectodami/SeguimientoActivity.kt
+++ app/src/main/java/com/cibertec/proyectodami/SeguimientoActivity.kt
@@ -1,20 +1,18 @@
 package com.cibertec.proyectodami
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.cibertec.proyectodami.databinding.ActivitySeguimientoBinding
 
 class SeguimientoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySeguimientoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_seguimiento)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        binding = ActivitySeguimientoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
