cce1e714bd42b9968ad90f00d7653329a78dcc5a
Nothing <andy.v.centeno@gmail.com>
2025-10-24 10:36:38 -0500

    Implementacion del splash con animacion y imagenes del logo, validado si el usuario ya esta logeado

--- app/src/main/java/com/cibertec/proyectodami/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/cibertec/proyectodami/data/remote/RetrofitInstance.kt
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit
 object RetrofitInstance {
 
     /* Cambiar el local IP de tu red de wifi*/
-    private const val LOCAL_IP = "192.168.1.7"
+    private const val LOCAL_IP = "192.168.1.5"
     private const val PORT = "8080"
 
     fun create(userPreferences: UserPreferences): Retrofit {
